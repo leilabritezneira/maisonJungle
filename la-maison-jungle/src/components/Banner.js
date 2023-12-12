1513602769bbc8ddff7245f2253595a68bcf8183
Leila <britezneira.leila@gmail.com>
2023-12-12 23:16:09 +0100

    P2C2 - Incorporation of style and assets

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,6 +1,14 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+
 function Banner() {
-	const title = 'La maison jungle'
-	return <h1>{title}</h1>
+    const title = 'La maison jungle'
+    return (
+        <div className='lmj-banner'>
+            <img src={logo} alt='La maison jungle' className='lmj-logo' />
+            <h1 className='lmj-title'>{title}</h1>
+        </div>
+    )
 }
 
 export default Banner
