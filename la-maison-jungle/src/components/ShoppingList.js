e7998f95819ea7985ba98a4803f3b89c53605e2f
Leila <britezneira.leila@gmail.com>
2023-12-13 22:37:56 +0100

    P2C3 - List and conditions

---
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -0,0 +1,30 @@
+import { plantList } from '../datas/PlantList'
+import '../styles/ShoppingList.css'
+
+function ShoppingList() {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}
+						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default ShoppingList
